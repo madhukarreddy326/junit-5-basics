2f64454c1e0059b419a5fbceca868c36f4477755
Madhukar <madhukar.reddy326@gmail.com>
2021-05-22 21:07:19 +0530

    added method testAdd

--- src/test/java/com/rgp/MathUtilsTest.java
+++ src/test/java/com/rgp/MathUtilsTest.java
@@ -7,12 +7,12 @@ import static org.junit.jupiter.api.Assertions.*;
 class MathUtilsTest {
 
     @Test
-    void add() {
+    void testAdd() {
         //System.out.println("Tests are running");
        // fail("not yet implemented");
         MathUtils utils = new MathUtils();
         int expected = 2;
         int result = utils.add(1,1);
-        assertEquals(expected,result);
+        assertEquals(expected,result,"this method adds the two integers and return integer");
     }
 }
