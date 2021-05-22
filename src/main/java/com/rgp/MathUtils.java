62f203ae0d632a8f1b8f454392d18b4b7dde4fe2
Madhukar <madhukar.reddy326@gmail.com>
2021-05-22 20:58:48 +0530

    added method to test the method using assertEquals

--- src/main/java/com/rgp/MathUtils.java
+++ src/main/java/com/rgp/MathUtils.java
@@ -1,8 +1,10 @@
 package com.rgp;
+
 public class MathUtils {
 
     public int add(int a, int b)
     {
         return a+b;
     }
+
 }
--- src/test/java/com/rgp/MathUtilsTest.java
+++ src/test/java/com/rgp/MathUtilsTest.java
@@ -8,6 +8,11 @@ class MathUtilsTest {
 
     @Test
     void add() {
-        System.out.println("Tests are running");
+        //System.out.println("Tests are running");
+       // fail("not yet implemented");
+        MathUtils utils = new MathUtils();
+        int expected = 2;
+        int result = utils.add(1,1);
+        assertEquals(expected,result);
     }
 }
