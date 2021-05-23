e783b0ebb9ac3964c066a22801962fa573be158e
Madhukar <madhukar.reddy326@gmail.com>
2021-05-23 15:25:13 +0530

    @BeforeEach junit life cycle is added. which will be called before any test method.

--- src/test/java/com/rgp/MathUtilsTest.java
+++ src/test/java/com/rgp/MathUtilsTest.java
@@ -1,16 +1,23 @@
 package com.rgp;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class MathUtilsTest {
+    MathUtils utils;
 
+    @BeforeEach
+    void init() {
+
+        utils = new MathUtils();
+    }
     @Test
     void testAdd() {
         //System.out.println("Tests are running");
-       // fail("not yet implemented");
-        MathUtils utils = new MathUtils();
+      // fail("not yet implemented");
         int expected = 2;
         int result = utils.add(1,1);
         assertEquals(expected,result,"this method adds the two integers and return integer");
@@ -18,18 +25,12 @@ class MathUtilsTest {
     @Test
     void testDivide()
     {
-        MathUtils utils = new MathUtils();
         assertThrows(ArithmeticException.class,()->utils.divide(1,0),"Divide by 0 will leads Arithmetic exception");
-
-
     }
 
     @Test
     void testComputCircleArea()
     {
-        MathUtils utils = new MathUtils();
-        assertEquals(314.1592653589793,utils.computeCircleArea(10.0),"Method failure in the computeCircleRadius");
-
-
+       assertEquals(314.1592653589793,utils.computeCircleArea(10.0),"Method failure in the computeCircleRadius");
     }
 }
